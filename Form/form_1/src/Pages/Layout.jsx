1c2e3403a8e716a58fbd6ab0b6f47fc6cde30912
SAHILGOHIL1999 <sahil19160@gmail.com>
2026-04-27 23:33:26 +0530

    your message

---
+++ Form/form_1/src/Pages/Layout.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import Pages from "./Pages";
+import { BrowserRouter, Router, Route } from 'react-router-dom';
+import { Component } from '../components/Components';
---
+++ Form/form_1/src/Pages/Pages.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Home from './Home'
+import About from './About'
+import Contact from './Contact'
+import Projects from './Projects'
+import Service from './Services'
+
+const Pages = {
+    Home,
+    About,
+    Contact,
+    Projects,
+    Service 
+}
+
+export default Pages
