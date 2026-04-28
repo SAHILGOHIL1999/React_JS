dfe1401be35e3eaedf527709355becdd7a5537fb
SAHILGOHIL1999 <sahil19160@gmail.com>
2026-04-28 09:50:11 +0530

    file update

--- Form/form_1/src/App.jsx
+++ Form/form_1/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react';
 import './App.css';
+import Layout from "./Pages/Layout";
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    
+    <Layout />
     </>
   )
 }
--- Form/form_1/src/Pages/Layout.jsx
+++ Form/form_1/src/Pages/Layout.jsx
@@ -1,4 +1,22 @@
 import React from 'react'
 import Pages from "./Pages";
-import { BrowserRouter, Router, Route } from 'react-router-dom';
-import { Component } from '../components/Components';
+import { BrowserRouter , Routes , Route } from 'react-router-dom'
+import { Components } from '../components/Components';
+
+const Layout = () => {
+    return (
+        <BrowserRouter>
+            <Components.Navbar />
+            <Routes>
+                <Route path="/"
+                    element={<Pages.Home />} />
+                <Route path="/about"
+                    element={<Pages.About />} />
+                <Route path="/contact"
+                    element={<Pages.Contact />} />
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default Layout
