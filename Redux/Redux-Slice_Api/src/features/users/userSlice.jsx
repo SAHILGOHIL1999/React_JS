eef9be800a43d0a683a9c65661fb54eb7488076e
SAHILGOHIL1999 <sahil19160@gmail.com>
2026-05-12 18:42:16 +0530

    file update

---
+++ Redux/Redux-Slice_Api/src/features/users/userSlice.jsx
@@ -0,0 +1,12 @@
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from 'axios'
+
+const initalState = {
+    user : [],
+    loading : false,
+    error: null
+}
+
+// API Calling
+
+export const fe
