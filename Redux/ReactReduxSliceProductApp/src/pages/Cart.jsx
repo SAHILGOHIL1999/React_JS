6a60b8cad52dedb24e2663feddc5e3023865f5c6
SAHILGOHIL1999 <sahil19160@gmail.com>
2026-05-16 22:37:04 +0530

    file update

---
+++ Redux/ReactReduxSliceProductApp/src/pages/Cart.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useSelector , useDispatch } from 'react-redux'
+import ProductCard from '../components/ProductCard'
+
+const Cart = () => {
+
+  const cartData = useSelector((state) => state.cart.cartItem)  
+
+  // console.log("cartData" , cartData)
+
+  return (
+    <>
+    <div>Cart</div>
+    <ProductCard productData={cartData}/>
+    </>
+  )
+}
+
+export default Cart
---
+++ Redux/ReactReduxSliceProductApp/src/store.jsx
@@ -0,0 +1,12 @@
+import { configureStore } from "@reduxjs/toolkit";
+import productReducer from './features/products/productSlice'
+import cartReducer from './features/cart/cartSlice'
+import wishlistReducer from './features/cart/wishSlice'
+
+export const store = configureStore({
+  reducer:{
+   product:productReducer,
+   cart:cartReducer,
+   wishlist:wishlistReducer 
+  }
+})
